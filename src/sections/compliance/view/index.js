62b499b56544a76b93890315888a6b96ed55a19c
rutvik <rutvikpatel91037@gmail.com>
2023-09-30 17:18:20 +0530

    compliance chechker module setup

--- src/sections/compliance/view/index.js
+++ src/sections/compliance/view/index.js
@@ -1,3 +1,1 @@
 export { default as ComplianceListView } from './compliance-list-view';
-export { default as ComplianceEditView } from './compliance-edit-view';
-export { default as ComplianceDetailsView } from './compliance-details-view';
--- src/utils/axios.js
+++ src/utils/axios.js
@@ -26,28 +26,9 @@ export const fetcher = async (args) => {
 // ----------------------------------------------------------------------
 
 export const endpoints = {
-  chat: '/api/chat',
-  kanban: '/api/kanban',
-  calendar: '/api/calendar',
   auth: {
     me: '/api/auth/me',
     login: '/api/auth/login',
     register: '/api/auth/register',
-  },
-  mail: {
-    list: '/api/mail/list',
-    details: '/api/mail/details',
-    labels: '/api/mail/labels',
-  },
-  post: {
-    list: '/api/post/list',
-    details: '/api/post/details',
-    latest: '/api/post/latest',
-    search: '/api/post/search',
-  },
-  product: {
-    list: '/api/product/list',
-    details: '/api/product/details',
-    search: '/api/product/search',
   },
 };
